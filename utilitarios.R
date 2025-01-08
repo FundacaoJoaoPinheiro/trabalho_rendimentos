d02de1666d2cb7b2f3e6f076e875654199c250c6
João Paulo G. Garcia <joaopauloggarcia@gmail.com>
2025-01-08 13:18:05 -0300

    lista com os códigos de variáveis por tabela completa.

--- utilitarios.R
+++ utilitarios.R
@@ -1,6 +1,6 @@
 #=========== EM ELABORAÇÃO. NÃO UTILIZAR =================#
 
-# Reproduz tabelas SIDRA da PNADC/A, 5a visita, recortando os dados
+# Reproduz tabelas SIDRA da PNADC/A, 5a visita, filtrando dados
 # para os dez estratos geográficos de Minas Gerais.
 # Tema das tabelas: Rendimento de todas as fontes.
 #
@@ -15,82 +15,99 @@ pnadc_dir <- "Microdados"  # pasta com os arquivos da PNADC
 pnadc_ano <- 2023
 
 codvar <- list(
-	"7426", = c("VD4020", "VD4020", "V5004A", "V5007A", "V5006A", "V5008A"), #F
-	"7427", = c("VD5002"),
-	"7428", = c("VD5002"),
-	"7429", = c(),
-	"7430", = c(),
-	"7431", = c(),  #F
-	"7432", = c(),  #F
-	"7433", = c(),  #F
-	"7434", = c(),  #F
-	"7435", = c(),
-	"7436", = c(),  #F
-	"7437", = c(),
-	"7438", = c(),
-	"7439", = c(),  #F
-	"7440", = c(),  #F
-	"7441", = c(),
-	"7442", = c(),
-	"7443", = c(),
-	"7444", = c(),
-	"7445", = c(),
-	"7446", = c(),
-	"7447", = c(),  #F
-	"7448", = c(),  #F
-	"7449", = c(),  #F
-	"7450", = c(),  #F
-	"7451", = c(),  #F
-	"7452", = c(),  #F
-	"7453", = c(),
-	"7454", = c(),  #F
-	"7455", = c(),  #F
-	"7456", = c(),  #F
-	"7457", = c(),  #F
-	"7458", = c(),
-	"7521", = c(),  #F
-	"7526", = c(),
-	"7527", = c(),
-	"7529", = c(),
-	"7530", = c(),
-	"7531", = c(),
-	"7532", = c(),
-	"7533", = c(),
-	"7534", = c(),
-	"7535", = c(),
-	"7536", = c(),  #F
-	"7537", = c(),
-	"7538", = c(),
-	"7539", = c(),
-	"7540", = c(),  #F
-	"7541", = c(),
-	"7542", = c(),
-	"7543", = c(),
-	"7544", = c(),
-	"7545", = c(),
-	"7546", = c(),  #F
-	"7547", = c(),
-	"7548", = c(),
-	"7549", = c(),
-	"7550", = c(),  #F
-	"7551", = c(),
-	"7552", = c(),
-	"7553", = c(),
-	"7554", = c(),
-	"7559", = c(),
-	"7560", = c(),
-	"7561", = c(),
-	"7562", = c(),
-	"7563", = c(),
-	"7564", = c() 
+	`7426` = c("VD4019", "VD4020", "V5004A", "V5007A", # não requer deflator
+               "V5006A", "V5008A", "V5005A"),
+	`7427` = c("VD5011"),
+	`7428` = c("VD5011"),
+	`7429` = c("VD5011", "VD4020", "V5004A",
+	           "V5007A", "V5006A", "V5008A"),
+	`7430` = c("VD4019", "VD4020"),
+	`7431` = c("V2010", "V4009", "V4033"),     # não requer deflator
+	`7432` = c("V2009", "V4009", "V4033"),     # não requer deflator
+	`7433` = c("VD3004", "V4009", "V4033"),    # não requer deflator
+	`7434` = c("V2007", "V4009", "V4033"),     # não requer deflator
+	`7435` = c("VD5011"),
+	`7436` = c(),
+	`7437` = c("VD4019", "VD4020", "V5004A2", "V4007A2",
+	           "V5006A2", "V5008A2", "V5005A2"),
+	`7438` = c("VD5011"),
+	`7439` = c("V2005", "V4034"),              # não requer deflator
+	`7440` = c("V1023", "V2005", "V4034"),     # não requer deflator
+	`7441` = c("V2010", "VD4019", "VD4020"),
+	`7442` = c("VD4019", "VD4020"),
+	`7443` = c("VD3004", "VD4019", "VD4020"),
+	`7444` = c("V2007", "VD4019", "VD4020"),
+	`7445` = c("V1023", "VD4019", "VD4020"),
+	`7446` = c("V2005", "VD4019", "VD4020"),
+	`7447` = c("VD3004", "V5002A"),    # não requer deflator
+	`7448` = c("VD3004", "V5002A"),    # não requer deflator
+	`7449` = c("V5002A"),        # não requer deflator
+	`7450` = c("V5002A"),        # não requer deflator
+	`7451` = c("V5001A"),        # não requer deflator
+	`7452` = c("V5001A"),        # não requer deflator
+	`7453` = c("VD4019"),
+	`7454` = c("VD3004", "V5001A"),    # não requer deflator
+	`7455` = c("VD3004", "V5001A"),    # não requer deflator
+	`7456` = c("V2001", "V5001A", "V5002A", "V5003A"),    # não requer deflator
+	`7457` = c("V5001A", "V5002A", "V5003A"),             # não requer deflator
+	`7458` = c("VD5011", "V5001A", "V5002A", "V5003A"),
+	`7521` = c("VD5011"),
+	`7526` = c("VD5011"),
+	`7527` = c("VD5011"),
+	`7529` = c("VD5011"),
+	`7530` = c("VD5011"),
+	`7531` = c("VD5011"),
+	`7532` = c("VD5011"),
+	`7533` = c("VD5011"),
+	`7534` = c("VD5011"),
+	`7535` = c("VD4019"),
+	`7536` = c("VD4019"),
+	`7537` = c("VD4019"),
+	`7538` = c("VD4019"),
+	`7539` = c("VD4019"),
+	`7540` = c("VD4019"),
+	`7541` = c("VD4019"),
+	`7542` = c("VD4019"),
+	`7543` = c("VD4019"),
+	`7544` = c("VD4019"),
+	`7545` = c("VD4019"),
+	`7546` = c("VD4019"),
+	`7547` = c("VD4019"),
+	`7548` = c("VD4019"),
+	`7549` = c("VD4019"),
+	`7550` = c("VD4019"),
+	`7551` = c("VD4019"),
+	`7552` = c("VD4019"),
+	`7553` = c("VD4019"),
+	`7554` = c("VD4019"),
+	`7559` = c("VD4019"),
+	`7560` = c("VD4019"),
+	`7561` = c("VD5011"),
+	`7562` = c("VD4019"),
+	`7563` = c("VD4019"),
+	`7564` = c("VD5011")
 )
 
+estratos_geo <- c(
+    "Belo Horizonte (MG)",     # 3110,
+	"Entorno metropol. de Belo Horizonte (MG)",    # 3120,
+	"Colar metropolitano de Belo Horizonte (MG)",  # 3130,
+	"Integrada de Brasília em Minas Gerais",       # 3140,
+	"Sul de Minas Gerais",     # 3151,
+	"Triângulo Mineiro",       # 3152,
+	"Zona da Mata (MG)",       # 3153,
+	"Norte de Minas Gerais",   # 3154,
+	"Vale do Rio Doce (MG)",   # 3155,
+	"Central de Minas Gerais"  # 3156
+)
+
+#----------------------------------------------------------
 # FUNÇÕES
 
-# Gerar plano amostral para MG, adicionando estratos geográficos.
+# Gerar desenho amostral para MG, adicionando estratos geográficos.
 # `tabelas` : um vetor com número de tabelas, cujas variáveis serão importadas;
 # `download`: um argumento lógico que define se a importação será online
-gerar_PA <- function(tabelas = NULL, download = FALSE) {
+gerar_DA <- function(tabelas = NULL, download = FALSE) {
 
 	# definir variáveis com base nas tabelas passadas como argumentos
 	if (is.null(tabelas)) {
@@ -103,7 +120,7 @@ gerar_PA <- function(tabelas = NULL, download = FALSE) {
 	# tabelas com estimativas para população (não precisam de deflator)
 	tabelas_pop <- c("7426", "7431", "7432", "7433", "7434", "7436", "7439",
 		"7440", "7447", "7448", "7449", "7450", "7451", "7452", "7454", "7455",
-		"7456", "7457", "7521", "7536", "7540", "7546", "7550")
+		"7456", "7457")
 	
 	# incorporar deflatores de acordo com as tabelas desejadas (TRUE ou FALSE)
 	requer_deflator <- length(setdiff(tabelas, tabelas_pop)) > 0
@@ -113,27 +130,35 @@ gerar_PA <- function(tabelas = NULL, download = FALSE) {
 		dados <- get_pnadc(
 			year = pnadc_ano,
 			interview = 5,
-			design = FALSE,    # será feito pela função pnadc_design
-			vars = variaveis,
+			design = FALSE,  # será feito pela função pnadc_design
+			vars = c(variaveis, "UF", "V2009"),  # sempre importar UF e Idade
 			deflator = requer_deflator
 		)
 	} else {
 		# definir caminho para os arquivos relevantes
-		microdados <- file.path(pnadc_dir, paste0("PNADC_",
-			pnadc_ano, "_visita5.txt"))
-		input <- file.path(pnadc_dir, paste0("input_PNADC_",
-			pnadc_ano, "_visita5.txt"))
-		dicionario <- file.path(pnadc_dir, paste0("dicionario_PNADC_microdados_",
-			pnadc_ano, "_visita5.xls"))
-		deflator <- file.path(pnadc_dir, paste0("deflator_PNADC_",
-			pnadc_ano, ".xls"))
+		microdados <- file.path(
+			pnadc_dir,
+			paste0("PNADC_", pnadc_ano, "_visita5.txt")
+		)
+		input <- file.path(
+			pnadc_dir,
+			paste0("input_PNADC_", pnadc_ano, "_visita5.txt")
+		)
+		dicionario <- file.path(
+			pnadc_dir,
+			paste0("dicionario_PNADC_microdados_",pnadc_ano, "_visita5.xls")
+		)
+		deflator <- file.path(
+			pnadc_dir,
+			paste0("deflator_PNADC_",pnadc_ano, ".xls")
+		)
 
-		# ler os arquivos e gerar o plano amostral
+		# ler os arquivos e gerar o desenho amostral
 		dados <- pnadc_labeller(
 			data_pnadc = read_pnadc(
 				microdata = microdados,
 				input = input,
-				vars = variaveis
+				vars = c(variaveis, "UF", "V2009")  # sempre importar UF e Idade
 			),
 			dictionary.file = dicionario
 		)
@@ -141,7 +166,7 @@ gerar_PA <- function(tabelas = NULL, download = FALSE) {
 			dados <- pnadc_deflator(dados, deflator.file = deflator)
 		}
 	}
-	dados <- pnadc_design(subset(dados, UF == "Minas Gerais"))
+	dados <- pnadc_design(subset(dados, UF == "Minas Gerais", V2009 >= 14))
 
 	# adicionar coluna com os códigos dos Estratos Geográficos
 	dados$variables <- transform(
@@ -153,10 +178,12 @@ gerar_PA <- function(tabelas = NULL, download = FALSE) {
 }
 
 # Função para estimar totais por variável e estrato geográfico
-# `PA` é o plano amostral (svyrep.design)
+# `DA` é o desenho amostral (svyrep.design)
 # `var` é uma variável/coluna (string)
-estimar_pop <- function(PA, var) {
-	formula <- as.formula(paste0("~interaction(Estrato_G, ", var, ")"))
-  	svytotal(x = formula, design = PA, na.rm = TRUE)
-}
+#gerar_tabela <- function(DA, tab) {
+#	formula <- as.formula(paste0("~", var))
+#  	tabela <- svyby(formula, by=~Estrato_G, FUN = svytotal, vartype = "cv",
+#  		design = DA, keep.names = TRUE, na.rm = TRUE)
+#	return(tabela)
+#}
 
