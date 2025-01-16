21e31f8e2fe2d342c59bfab9ed32f0f2a5f85a82
João Paulo G. Garcia <joaopauloggarcia@gmail.com>
2025-01-16 10:54:45 -0300

    pequenas correções.

--- testes/rascunho_RDPC.R
+++ testes/rascunho_RDPC.R
@@ -12,29 +12,13 @@ pnadc_ano = 2023
 visita = 1
 pnadc_dir = "Microdados"
 
-if (file.exists("desenho_amostral.RDS")) {
-	desenho_amostral <- readRDS("desenho_amostral.RDS")
+if (file.exists("desenho_ocupadas.RDS")) {
+	desenho_amostral <- readRDS("desenho_ocupadas.RDS")
 } else {
-	microdados <- list.files(
-		pnadc_dir,
-		paste0("^PNADC_", pnadc_ano, "_visita", visita, ".*txt$"),
-		full.names = TRUE
-	)
-	input <- list.files(
-		pnadc_dir,
-		paste0("^input_PNADC_", pnadc_ano, "_visita", visita, ".*txt$"),
-		full.names = TRUE
-	)
-	dicionario <- list.files(
-		pnadc_dir,
-		paste0("^dicionario_PNADC_microdados_", pnadc_ano,
-			"_visita", visita, ".*xls$"),
-		full.names = TRUE
-	)
-	deflator <- file.path(
-		pnadc_dir,
-		paste0("deflator_PNADC_", pnadc_ano, ".xls")
-	)
+	microdados <- "Microdados/PNADC_2023_visita1.txt"
+	input <- "Microdados/input_PNADC_2023_visita1_20241220.txt"
+	dict <- "Microdados/dicionario_PNADC_microdados_2023_visita1_20241220.xls"
+	deflator <- "Microdados/deflator_PNADC_2023.xls"
 	desenho_amostral <- pnadc_deflator(
 		pnadc_labeller(
 			data_pnadc = read_pnadc(
@@ -42,7 +26,7 @@ if (file.exists("desenho_amostral.RDS")) {
 				input = input,
 				vars = c(variaveis, "UF", "V2009")  # sempre importar UF e Idade
 			),
-			dictionary.file = dicionario
+			dictionary.file = dict
 		),
 		deflator.file = deflator
 	)
@@ -50,9 +34,14 @@ if (file.exists("desenho_amostral.RDS")) {
 		subset(desenho_amostral, UF == "Minas Gerais")
 	)
 }
+	
+desenho_amostral$variables <- transform(
+	desenho_amostral$variables,
+	Estrato_G = factor(substr(Estrato, 1, 4))
+)
 
 # 2) Rendimento Domiciliar Per Capita a preços médios do último/próprio ano
-# 7427(7428), 7526(7438), 7458, 7529(7521), (7527), (7530), 7533(7531),
+# 7427(7428), 7429, 7526(7438), 7458, 7529(7521), (7527), (7530), 7533(7531),
 # 7534(7532), 7564(7561) --------------------------------------------------
 
 # as 3 categorias abaixos são excluídas no cálculo da rendimento domiciliar
@@ -62,27 +51,32 @@ desenho_amostral$variables <- transform(
 		V2005 == "Pensionista" |
 		V2005 == "Empregado(a) doméstico(a)" |
 		V2005 == "Parente do(a) empregado(a) doméstico(a)",
-		0, 1
+		NA, 1
 	)
 )
 
 # moradores por domicílio incluídos no cálculo do rendimento domiciliar
 desenho_amostral$variables <- transform(
 	desenho_amostral$variables,
-	V2001.Incluidos = ave(V2005.Incluidas, ID_DOMICILIO, FUN = sum)
+	V2001.Incluidos = ave(
+		V2005.Incluidas,
+		ID_DOMICILIO,
+		FUN = function(x) sum(x, na.rm=T)
+	)
 )
 
 # ÚLTIMO ANO -----------------------
+
 # rendimento habitual de todos os trabalhos e efetivo de outras fontes
 # a preços médios do último ano (obs: em 2023 CO1 = CO2 e CO1e = CO2e)
 desenho_amostral$variables <- transform(
 	desenho_amostral$variables,
 	VD4019.Real2 = ifelse(
-		is.na(VD4019) | V2005.Incluidas == 0 | V2009 < 14,
+		is.na(VD4019) | is.na(V2005.Incluidas) | V2009 < 14,
 		0, VD4019 * CO2
 	),
 	VD4048.Real2 = ifelse(
-		is.na(VD4048) | V2005.Incluidas == 0,
+		is.na(VD4048) | is.na(V2005.Incluidas),
 		0, VD4048 * CO2e
 	)
 )
@@ -101,8 +95,9 @@ desenho_amostral$variables <- transform(
 desenho_amostral$variables <- transform(
 	desenho_amostral$variables,
 	VD5008.Real2 = ifelse(
-		V2005.Incluidas == 0,
-		0, VD5007.Real2 / V2001.Incluidos)
+		is.na(V2005.Incluidas),
+		0, VD5007.Real2 / V2001.Incluidos
+	)
 )
 
 # 7526 - pequenas diferenças nos últimos dois quantis
@@ -119,7 +114,7 @@ quantis2 <- limites_RDPC2[[1]][, 1]
 print(limites_RDPC2)
 unname(sidra_7526[c(7,3)])
 
-# 7529 - valores bem parecidos, diferença maior em P10 - P20
+# 7529 - valores bem parecidos, diferença maior em P5 - P10
 sidra_7529 <- get_sidra(x = 7529, variable = 606, period = "2023",
 	geo = "State", geo.filter = 31, header = TRUE, format = 2)
 sidra_7529 <- transform(sidra_7529, Valor = Valor * 1000)
@@ -155,13 +150,14 @@ pop_CSP_RDPC2 <- svyby(
 	~V2005.Incluidas,
 	~CSP.VD5008.Real2,
 	desenho_amostral,
-	svytotal
+	FUN = svytotal,
+	na.rm = TRUE
 )
 
 print(pop_CSP_RDPC2)
 unname(sidra_7529[c(7,3)])
 
-# 7564
+# 7564 - evidentemente, o mesmo de 7529
 sidra_7564 <- get_sidra(x = 7564, variable = 606, period = "2023",
 	geo = "State", geo.filter = 31, header = TRUE, format = 2)
 sidra_7564 <- transform(sidra_7564, Valor = Valor * 1000)
@@ -174,12 +170,20 @@ pop_CAP_RDPC2 <- data.frame(
 	Populacao = cumsum(pop_CSP_RDPC2[[2]])
 )
 
+print(pop_CAP_RDPC2)
+unname(sidra_7564[c(7,3)])
+
 # 7427 - valores parecidos, mas a maioria é diferente
 sidra_7427 <- get_sidra(x = 7427, variable = 10495, period = "2023",
 	geo = "State", geo.filter = 31, header = TRUE, format = 2)
 sidra_7427 <- transform(sidra_7427, Valor = Valor * 10^6)
 
-massa_RDCP2 <- svyby(~VD5008.Real2, ~CSP.VD5008.Real2, desenho_amostral, svytotal)
+massa_RDCP2 <- svyby(
+	~VD5008.Real2,
+	~CSP.VD5008.Real2,
+	desenho_amostral,
+	FUN = svytotal
+)
 
 print(massa_RDCP2)
 unname(sidra_7427[c(7,3)])
@@ -192,7 +196,7 @@ RMeDPC2_CSP <- svyby(
 	~VD5008.Real2,
 	~CSP.VD5008.Real2,
 	desenho_amostral,
-	svymean
+	FUN = svymean
 )
 unname(sidra_7533[c(7,3)])
 
@@ -202,25 +206,44 @@ sidra_7534 <- get_sidra(x = 7534, variable = 10816, period = "2023",
 
 RMeDPC2_CAP <- data.frame(
 	Classes.Acumuladas = rotulos_CAP,
-	RMe.Dom.Per.Capita = cumsum(pop_CSP_RDPC2[[2]] * RMeDPC2_CSP[[2]]) /
-                         cumsum(pop_CSP_RDPC2[[2]])
+	RMe.Dom.Per.Capita =
+		cumsum(RMeDPC2_CSP[[2]] * pop_CSP_RDPC2[[2]]) / # Somatório (RMe x pop) /
+			cumsum(pop_CSP_RDPC2[[2]])                  # Somatório (pop)
 )
 
 print(RMeDPC2_CAP)
 unname(sidra_7534[c(7,3)])
 
+# 7429
+
+# 7458
+sidra_7458 <- get_sidra(x = 7458, variable = 10816, period = "2023",
+	geo = "State", geo.filter = 31, header = TRUE, format = 2)
+
+RMeDPC2_prog_social <- svybys(
+	~VD5008.Real2,
+	~V5001A + V5002A + V5003A,
+	desenho_amostral,
+	FUN = svymean
+)
+
+print(RMeDPC2_prog_social)
+unname(sidra_7458[c(7,3)])
+
 # PRÓPRIO ANO -----------------------
+# Observação: no ano mais recente, não há diferença entre os deflatores
+# do último ano e do próprio ano.
 
 # rendimento habitual de todos os trabalhos e efetivo de outras fontes
-# a preços médios do último ano
+# a preços médios do ano
 desenho_amostral$variables <- transform(
 	desenho_amostral$variables,
 	VD4019.Real1 = ifelse(
-		is.na(VD4019) | V2005.Incluidas == 0 | V2009 < 14,
+		is.na(VD4019) | is.na(V2005.Incluidas) | V2009 < 14,
 		0, VD4019 * CO2
 	),
 	VD4048.Real1 = ifelse(
-		is.na(VD4048) | V2005.Incluidas == 0,
+		is.na(VD4048) | is.na(V2005.Incluidas),
 		0, VD4048 * CO2e
 	)
 )
@@ -239,7 +262,7 @@ desenho_amostral$variables <- transform(
 desenho_amostral$variables <- transform(
 	desenho_amostral$variables,
 	VD5008.Real1 = ifelse(
-		V2005.Incluidas == 0,
+		is.na(V2005.Incluidas),
 		0, VD5007.Real1 / V2001.Incluidos)
 )
 
@@ -250,8 +273,7 @@ sidra_7438 <- get_sidra(x = 7438, variable = 10769, period = "2023",
 limites_RDPC1 <- svyquantile(
 	~VD5008.Real1,
 	desenho_amostral,
-	quantiles = c(0.05, seq(0.10, 0.90, by = 0.10), 0.95, 0.99),
-	na.rm = TRUE
+	quantiles = c(0.05, seq(0.10, 0.90, by = 0.10), 0.95, 0.99)
 )
 quantis1 <- limites_RDPC1[[1]][, 1]
 
@@ -278,7 +300,8 @@ pop_CSP_RDPC1 <- svyby(
 	~V2005.Incluidas,
 	~CSP.VD5008.Real1,
 	desenho_amostral,
-	svytotal
+	FUN = svytotal,
+	na.rm = TRUE
 )
 
 print(pop_CSP_RDPC1)
@@ -289,7 +312,12 @@ sidra_7428 <- get_sidra(x = 7428, variable = 10490, period = "2023",
 	geo = "State", geo.filter = 31, header = TRUE, format = 2)
 sidra_7428 <- transform(sidra_7428, Valor = Valor * 10^6)
 
-massa_RDCP1 <- svyby(~VD5008.Real1, ~CSP.VD5008.Real1, desenho_amostral, svytotal)
+massa_RDCP1 <- svyby(
+	~VD5008.Real1,
+	~CSP.VD5008.Real1,
+	desenho_amostra
+	FUN = , svytotal
+)
 
 print(massa_RDCP1)
 unname(sidra_7428[c(7,3)])
@@ -332,18 +360,3 @@ distribuicao_acumulada_RDCP1 <- data.frame(
 	Classes.Acumuladas = rotulos_CAP,
 	Distribuicao.Acumulada.RDCP1 = cumsum(distribuicao_simples_RDCP1[[2]])
 )
-
-# 7458
-sidra_7458 <- get_sidra(x = 7458, variable = 10816, period = "2023",
-	geo = "State", geo.filter = 31, header = TRUE, format = 2)
-
-RMeDPC2_prog_social <- svybys(
-	~VD5008.Real2,
-	~V5001A + V5002A + V5003A,
-	desenho_amostral,
-	svymean,
-	na.rm = TRUE
-)
-
-print(RMeDPC2_prog_social)
-unname(sidra_7458[c(7,3)])
